75c563e816d218b7cd03f581dd9bc0dc0f0d0fb8
Amine Yagoub <yagoub.2.amine@gmail.com>
2020-11-20 14:52:06 +0100

    change the package name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import SecEnv from './src/SecEnv'
 
-const secenv = (options = {}) => Object.freeze(
+const swEnv = (options = {}) => Object.freeze(
   new SecEnv(options)
 )
-module.exports = secenv
+module.exports = swEnv
